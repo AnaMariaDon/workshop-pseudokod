662820f02f904acc8cee9a7170fcf37c06993ef7
AnaMariaDonosoContreras <anamaria.donosocontreras@student.kyh.se>
2025-09-24 12:18:06 +0200

    who ate the cookie

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Century From Year
+/*Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.*/
+
 //MÅL: En funktion som tar in årtal och returnerar århundrade.
 
 //INPUTS: Årtal
@@ -7,3 +14,31 @@
 // Skriv en funktion som tar in årtal.
 // Beräkna årtal/100 och avrundar uppåt
 //Returnerar århundrade
+
+
+//Who ate the cookie?
+
+/* Description :
+ For this problem you must create a program that says who ate the last cookie. 
+ If the input is a string then "Zach" ate the cookie. If the input is a float or an int 
+ then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. 
+ The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach!
+(The reason you return Zach is because the input is a string)*/
+
+
+
+//MÅL: Ta reda på vem som åt sista kakan. Avgöra vilken input (datatyp) det är.
+
+//INPUT: string, siffra (float, int) eller övrigt.
+
+//RETURN: "Who ate the last cookie? It was + (namn)!"
+
+//STEG1: function vemAtDenSistaKakan (input)
+
+//Steg2: Om input är en sträng; så är namn = Zach 
+// annars om input är en siffra; sä är namn = Monica
+// annars namn = hunden
+
+//Steg3: Return "Who ate the last cookie? It was + (namn)!"
