f838afcf370ede6e34cee8d98f37a478488d2197
AnaMariaDonosoContreras <anamaria.donosocontreras@student.kyh.se>
2025-09-24 10:40:23 +0200

    Century from Year

---
+++ script.js
@@ -0,0 +1,9 @@
+//MÅL: En funktion som tar in årtal och returnerar århundrade.
+
+//INPUTS: Årtal
+
+//RETURN: Århundrade
+
+// Skriv en funktion som tar in årtal.
+// Beräkna årtal/100 och avrundar uppåt
+//Returnerar århundrade
